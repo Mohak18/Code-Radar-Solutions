1102142b6acf72d2bc93eb5dc8b61c5ddc710b3f
Mohak Taneja <mohaktaneja18vk@gmail.com>
2025-03-18 21:44:10 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,6 +1,6 @@
 // Your code here...
 // Function to sort an array in ascending order
-void sortArray(int arr[], int size) {
+void bubbleSort(int arr[], int size) {
     int temp;
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
